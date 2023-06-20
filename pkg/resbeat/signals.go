190c4ef248742dd137d949947707427aa75e1e91
Roman Glushko <roman.glushko.m@gmail.com>
2023-06-20 23:53:56 +0300

    Covered signal handler by tests

--- pkg/resbeat/signals.go
+++ pkg/resbeat/signals.go
@@ -6,10 +6,20 @@ import (
 	"os"
 	"os/signal"
 	"resbeat/pkg/resbeat/telemetry"
+	"sync"
 	"syscall"
 )
 
 type SignalHandler struct {
+	SigC chan os.Signal
+	wg   *sync.WaitGroup
+}
+
+func NewSignalHandler() *SignalHandler {
+	return &SignalHandler{
+		SigC: make(chan os.Signal, 1),
+		wg:   &sync.WaitGroup{},
+	}
 }
 
 func (h *SignalHandler) Handle(ctx context.Context) context.Context {
@@ -17,17 +27,19 @@ func (h *SignalHandler) Handle(ctx context.Context) context.Context {
 	// https://medium.com/@matryer/make-ctrl-c-cancel-the-context-context-bd006a8ad6ff
 	ctx, cancel := context.WithCancel(ctx)
 
+	h.wg.Add(1)
+
 	go func() {
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		signal.Notify(h.SigC, syscall.SIGINT, syscall.SIGTERM)
 
 		defer func() {
-			signal.Stop(sigs)
+			signal.Stop(h.SigC)
 			cancel()
+			h.wg.Done()
 		}()
 
 		select {
-		case sig := <-sigs:
+		case sig := <-h.SigC:
 			logger.Info(fmt.Sprintf("resbeat received signal (%v), terminating", sig))
 			cancel()
 		case <-ctx.Done():
